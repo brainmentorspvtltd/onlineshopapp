adde431c57c99e249f5e7cfbf5841a06f21806ef
brainmentorspvtltd <brainmentorspvtltd@gmail.com>
2018-10-16 11:31:45 +0530

    Code2

---
+++ precache-manifest.ef24c84efdab1a4dfdae15f7be6bedb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "325c0566f3c35dcf41c4",
+    "url": "/onlineshopapp/static/js/runtime~main.325c0566.js"
+  },
+  {
+    "revision": "a6de2a2d625125af2fee",
+    "url": "/onlineshopapp/static/js/main.a6de2a2d.chunk.js"
+  },
+  {
+    "revision": "37f936a67bc210a54255",
+    "url": "/onlineshopapp/static/js/1.37f936a6.chunk.js"
+  },
+  {
+    "revision": "a6de2a2d625125af2fee",
+    "url": "/onlineshopapp/static/css/main.53d1c09a.chunk.css"
+  },
+  {
+    "revision": "37f936a67bc210a54255",
+    "url": "/onlineshopapp/static/css/1.292a6172.chunk.css"
+  },
+  {
+    "revision": "989ffad6b9ee6a8ec691a0c25cc3a90b",
+    "url": "/onlineshopapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e12bd43e2fa077dbd162d096e5bb820.js"
+  "/onlineshopapp/precache-manifest.ef24c84efdab1a4dfdae15f7be6bedb2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/onlineshopapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
